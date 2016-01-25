c1c614fc372873a71c1bbcad1da1e6d08c8a8eec
Steven Gann <sgann2012@fau.edu>
2016-01-25 07:41:30 -0500

    Defined Status Register
    Finally defined the first 7 bits of the status register, and added a
    StatusRegister class to the emulator. Inherits from Register, but with
    additional methods for cleaner access to the specific bits, and a
    binary-styled ToString method.

--- SG16/Register.cs
+++ SG16/Register.cs
@@ -11,6 +11,8 @@ namespace SG16
         public byte UpperByte = 0x00;
         public byte LowerByte = 0x00;
 
+        
+
         public void Increment(uint d)
         {
             if ((d + (uint)LowerByte) <= 255)
@@ -63,11 +65,44 @@ namespace SG16
 
         new public string ToString()
         {
-            string result = "";
             byte[] data = new byte[2];
             data[0] = UpperByte;
             data[1] = LowerByte;
             return ASM.ByteArrayToString(data);
+        }
+
+        public static bool GetBit(byte b, int bitNumber)
+        {
+            return (b & (1 << bitNumber)) != 0;
+        }
+
+        public static byte SetBit(byte b, int pos)
+        {
+            if (pos < 0 || pos > 7)
+                throw new ArgumentOutOfRangeException("pos", "Index must be in the range of 0-7.");
+
+            return (byte)(b | (1 << pos));
+        }
+
+        public static byte UnsetBit(byte b, int pos)
+        {
+            if (pos < 0 || pos > 7)
+                throw new ArgumentOutOfRangeException("pos", "Index must be in the range of 0-7.");
+
+            return (byte)(b & ~(1 << pos));
+        }
+
+        public static byte WriteBit(byte b, int pos, bool value)
+        {
+            if (value == true)
+            {
+                return SetBit(b, pos);
+            }
+            else
+            {
+                return UnsetBit(b, pos);
+            }
+
         }
     }
 }
---
+++ SG16/StatusRegister.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SG16
+{
+    public class StatusRegister : Register
+    {
+        public bool Z
+        {
+            get { return GetBit(LowerByte, 0); }
+            set { WriteBit(LowerByte, 0, value); }
+        }
+        public bool C
+        {
+            get { return GetBit(LowerByte, 1); }
+            set { WriteBit(LowerByte, 1, value); }
+        }
+        public bool N
+        {
+            get { return GetBit(LowerByte, 2); }
+            set { WriteBit(LowerByte, 2, value); }
+        }
+        public bool O
+        {
+            get { return GetBit(LowerByte, 3); }
+            set { WriteBit(LowerByte, 3, value); }
+        }
+        public bool P
+        {
+            get { return GetBit(LowerByte, 4); }
+            set { WriteBit(LowerByte, 4, value); }
+        }
+        public bool E
+        {
+            get { return GetBit(LowerByte, 5); }
+            set { WriteBit(LowerByte, 5, value); }
+        }
+        public bool L
+        {
+            get { return GetBit(LowerByte, 6); }
+            set { WriteBit(LowerByte, 6, value); }
+        }
+
+        new public string ToString()
+        {
+            string lower = Convert.ToString(LowerByte, 2).PadLeft(8, '0');
+            string upper = Convert.ToString(UpperByte, 2).PadLeft(8, '0');
+            return upper + lower;
+        }
+    }
+}
