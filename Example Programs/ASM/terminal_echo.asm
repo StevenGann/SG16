4777c5fc9984039b0234fad38c2b63173fdeef8a
Steven Gann <sgann2012@fau.edu>
2016-09-22 14:06:51 -0400

    Added terminal echo program
    This program demonstrates indirect addressing modes as well as
    techniques for building simple loops. I wrote this whole program blind,
    and some features used either aren't implemented in the assembler or
    emulator yet, or are just untested.
    
    Also redefined the ASCII literal to be a single character.

---
+++ Example Programs/ASM/terminal_echo.asm
@@ -0,0 +1,34 @@
+#terminal_echo.asm
+#Simple demonstration of input and output using the terminal.
+#Stores bytes from the terminal into an array. When it recieves a '.',
+#it echos the array back to the terminal. 
+
+START 							#0000	#Start of the program
+
+MOVE xFF00 RREF  				#0008	#Set RREF to the start address of the array
+MOVE x0000 $0000 				#0010	#Clear start address of array
+MOVE sR @FF00 					#0018 
+MOVE se @FF00 					#0020 
+MOVE sa @FF00 					#0028 
+MOVE sd @FF00 					#0030 
+MOVE sy @FF00 					#0038 
+MOVE s: @FF00 					#0040 
+MOVE x000A @FF00 # <new line> 	#0048 
+
+#Read and store loop
+EVAL @FF02  					#0050
+INCR $0000  					#0060	#Increment the value stored at the location RREF points to
+MOVE @FF02 $RREF  				#0068	#Copy the contents of FF02 into the location RREF points to, offset by the value stored at RREF
+COMP @FF02 s. 					#0070	#Check if the input is '.'
+MOVE x0000 @FF02 				#0078	#Reset the input
+
+
+#Print array to terminal loop
+MOVE x0000 USR0 				#0090	#Using USR0 as counter variable
+INCR USR0 						#0098	
+MOVE $USR0 @FF00 				#00A0	#Copy array element to output
+COMP USR0 $0000 				#00A8	
+
+MOVE x000A @FF00 # <new line>	#00B8
+
+END								#00C0
